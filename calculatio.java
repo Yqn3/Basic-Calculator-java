2332da9cd2bd9cebd39837d370dbfea7019a11bc
Yaz <58692811+Yqn3@users.noreply.github.com>
2021-07-25 06:00:03 +0400

    Create calculatio.java

---
+++ calculatio.java
@@ -0,0 +1,67 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Main
+{
+	public static void main(String[] args) {
+		
+		Scanner keyboard=new Scanner(System.in);
+		
+		char choose;
+		double num1 , num2 , multiply , dividing , positive ,negative , total;
+		
+		while(true){
+		System.out.println("Choose the type of operation: ");
+		System.out.println("1- Multiply * ");
+		System.out.println("2- Dividing / ");
+		System.out.println("3- positive + ");
+		System.out.println("4- negative - ");
+		System.out.print("Enter number from the list: ");
+		choose=keyboard.next().charAt(0);
+		
+		
+		if (choose == '1'){
+		    System.out.print("First Number: ");
+		    num1=keyboard.nextDouble();
+		    System.out.print("Second Number: ");
+		    num2=keyboard.nextDouble();
+		    total = num1 * num2;
+		    System.out.println("Total:" +total);
+		    System.out.print(num1 +" * " + num2 + " = " + total);
+		    System.out.println("\n");
+		}
+		if (choose == '2'){
+		    System.out.print("First Number: ");
+		    num1=keyboard.nextDouble();
+		    System.out.print("Second Number: ");
+		    num2=keyboard.nextDouble();
+		    total = num1 / num2;
+		    System.out.println("Total:" +total);
+		    System.out.print(num1 +" / " + num2 + " = " + total);
+		    System.out.println("\n");
+		}
+		if (choose == '3'){
+		    System.out.print("First Number: ");
+		    num1=keyboard.nextDouble();
+		    System.out.print("Second Number: ");
+		    num2=keyboard.nextDouble();
+		    total = num1 + num2;
+		    System.out.println("Total:" +total);
+		    System.out.print(num1 +" + " + num2 + " = " + total);
+		    System.out.println("\n");
+		}
+		if (choose == '4'){
+		    System.out.print("First Number: ");
+		    num1=keyboard.nextDouble();
+		    System.out.print("Second Number: ");
+		    num2=keyboard.nextDouble();
+		    total = num1 - num2;
+		    System.out.println("Total: " +total);
+		    System.out.print(num1 +" - " + num2 + " = " + total);
+		    System.out.println("\n");
+		}
+		}
+		
+	}
+}
